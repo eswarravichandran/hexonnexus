441dc691101ee258a0bb1550366ea938a5e0056e
eswarravichandran <eswarr0211@gmail.com>
2026-05-22 01:56:00 +0530

    Updated frontend files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,16 @@ import Home from './components/Home'
 import Login from './components/Login'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ProtectedRoutes from './routes/ProtectedRoutes'
+import { ToastContainer } from 'react-toastify';
 
 function App() {
     return (
         <>
+        <ToastContainer position="top-center" autoClose={5000}/>
         <Navbar/>
         <Routes>
-            <Route path='/' element={ <Home/> }/>
+            <Route path='/' element={ <ProtectedRoutes><Home/></ProtectedRoutes> }/>
             <Route path='/login' element={ <Login/> }/>
         </Routes>
         <Footer/>
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,12 +1,38 @@
 import React, { useState } from 'react'
 import '../styles/Login.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { toast } from 'react-toastify';
 
 const Login = () => {
 
+    const navigate = useNavigate();
     const [showTab, setShowTab] = useState(false);
-
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
     
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value,});
+    };
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        try {
+            const response = await axios.post("http://localhost:5000/api/auth/login", formData);
+            if(response.data) {
+                localStorage.setItem("token", response.data.token);
+                toast.success("Login Success!");
+                navigate("/");
+            }
+        }
+        catch (error) {
+            toast.error('Login failed!');
+        }
+    };
+
     return (
         <>
             <div className='login'>
@@ -16,21 +42,21 @@ const Login = () => {
                         <div className='login-form'>
                             <div>
                                 <label>Email</label>
-                                <input type='email' placeholder='Enter Email' required/>
+                                <input type='email' name='email' placeholder='Enter Email' onChange={handleChange} required/>
                             </div>
                             <div>
                                 <label>Password</label>
-                                <input type='password' placeholder='Enter Password' required/>
+                                <input type='password' name='password' placeholder='Enter Password' onChange={handleChange} required/>
                             </div>
                             <div>
-                                <button type='submit'>Login</button>
+                                <button type='submit' onClick={handleSubmit}>Login</button>
                                 <h4 onClick={() => setShowTab(!showTab)}>Forgot password? Click here</h4>
                             </div>
                             { showTab && (
                                 <>
                                 <div>
                                     <label>Please enter valid email</label>
-                                    <input type='email' placeholder='Enter Email' required/>
+                                    <input type='email' name='remail' placeholder='Enter Email' required/>
                                 </div>
                                 <div>
                                     <button type='submit'>Send Reset Link</button>
